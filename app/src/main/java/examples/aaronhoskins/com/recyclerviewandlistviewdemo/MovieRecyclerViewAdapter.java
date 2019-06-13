a12a1f0935c2e8e595d57a29a5584f874014c8ff
Aaron Hoskins <tobyhoskinsdev@gmail.com>
2019-06-13 12:15:04 -0400

    Glide Implementation introduced

--- app/src/main/java/examples/aaronhoskins/com/recyclerviewandlistviewdemo/MovieRecyclerViewAdapter.java
+++ app/src/main/java/examples/aaronhoskins/com/recyclerviewandlistviewdemo/MovieRecyclerViewAdapter.java
@@ -3,11 +3,14 @@ package examples.aaronhoskins.com.recyclerviewandlistviewdemo;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
@@ -39,6 +42,11 @@ public class MovieRecyclerViewAdapter
         holder.tvStudio.setText(itemsMovie.getStudio());
         holder.tvRating.setText(itemsMovie.getParentRating());
         holder.tvYear.setText(itemsMovie.getReleaseYear());
+        Glide
+                .with(holder.itemView)
+                .load("https://cdn.wallpapersafari.com/70/36/6mEV3A.jpg")
+                .into(holder.imgImage);
+
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,6 +71,7 @@ public class MovieRecyclerViewAdapter
         TextView tvRating;
         TextView tvStudio;
         TextView tvYear;
+        ImageView imgImage;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -71,6 +80,7 @@ public class MovieRecyclerViewAdapter
             tvRating = itemView.findViewById(R.id.tvRating);
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvStudio = itemView.findViewById(R.id.tvStudio);
+            imgImage = itemView.findViewById(R.id.imgImage);
         }
     }
 }
